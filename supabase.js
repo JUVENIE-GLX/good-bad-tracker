f37954d8a08b8fa10e77ae77958448b7aecd6991
JUVENIE <juvenie@users.noreply.github.com>
2026-06-10 23:17:22 +0800

    修复 Supabase 初始化代码

--- supabase.js
+++ supabase.js
@@ -3,8 +3,7 @@ const SUPABASE_URL = 'https://jqetyzffngnqxwjjzhsa.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpxZXR5emZmbmducXh3amp6aHNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwOTk4ODUsImV4cCI6MjA5NjY3NTg4NX0.MEtTUyVllHTLZydCY4uRht--PRv9NuJMmXuldrZ018E';
 
 // 初始化 Supabase 客户端
-const { createClient } = supabase;
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // 数据库表名
 const TABLE_NAME = 'events';
